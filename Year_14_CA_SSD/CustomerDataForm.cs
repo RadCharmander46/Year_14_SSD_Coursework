38a0d42f4d1ba5290ca88a765609b2cc31e15257
RadCharmander46 <emmettmckiernan08@gmail.com>
2025-11-20 20:19:07 +0000

    Added back loading customers on the Customer Data Form and testing how oush requests work

--- Year_14_CA_SSD/CustomerDataForm.cs
+++ Year_14_CA_SSD/CustomerDataForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,6 +14,8 @@ namespace Year_14_CA_SSD
     public partial class CustomerDataForm : Form
     {
         public event EventHandler AddCustomer;
+        public List<string[]> customers = new List<string[]>();
+        public List<int> displayedIndexes = new List<int>();
         public CustomerDataForm()
         {
             InitializeComponent();
@@ -44,6 +47,45 @@ namespace Year_14_CA_SSD
         private void Add_Customer_Button_Click(object sender, EventArgs e)
         {
 
+        }
+        void Load_Customers()
+        {
+            using (SqlConnection conn = new SqlConnection(Globals.connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "SELECT * FROM CustomerTable";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    int index = 0;
+                    while (reader.Read())
+                    {
+                        string[] customer = new string[reader.FieldCount];
+                        for(int i = 0; i < reader.FieldCount; i++)
+                        {
+                            if (reader.GetName(i) == "DateOfBirth")
+                            {
+                                customer[i] = Globals.removeTime(reader.GetValue(i).ToString());
+                            }
+                            else
+                            {
+                                customer[i] = reader.GetValue(i).ToString().Trim();
+                            }
+                        }
+                        displayedIndexes.Add(index);
+                        index++;
+
+                    }
+                    conn.Close();
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("An error occured");
+                }
+            }
         }
     }
     public class Add_Customer_EventArgs : EventArgs
